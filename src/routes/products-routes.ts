14fb85754bf0996c0512ca18424db04ab5f42c66
Wellington Barbosa <huehueb58@gmail.com>
2026-01-26 18:57:34 -0300

    corrige nome da instância do controlador de produtos

--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -2,13 +2,12 @@ import { ProductController } from "@/controllers/products-controller";
 import { Router, Request, Response, NextFunction } from "express";
 
 const productsRoutes = Router();
-
-const productController = new ProductController();
+const productsController = new ProductController();
 
 productsRoutes.get(
   "/",
   (request: Request, response: Response, next: NextFunction) => {
-    productController.index(request, response, next);
+    productsController.index(request, response, next);
   },
 );
 
