3f786f008dfc78ef3d424867fee0e5e60575531e
Wellington Barbosa <huehueb58@gmail.com>
2026-01-26 19:23:07 -0300

    adiciona rotas de produtos e classe AppError

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { productsRoutes } from "./routes/products-routes";
+import { routes } from "./routes/index";
 
 const app = express();
-app.use(express.json());
 const PORT = 3333;
 
-app.use(productsRoutes)
+app.use(express.json());
+app.use(routes)
 
 app.listen(PORT, () => 
   console.log(`Server is running on http://localhost:${PORT}`)
