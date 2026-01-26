08e82f97b745cdba130267e6b81f2b4d4406607b
Wellington Barbosa <huehueb58@gmail.com>
2026-01-26 19:35:35 -0300

    adiciona middleware de tratamento de erros

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import express from "express";
+
 import { productsRoutes } from "./routes/products-routes";
 import { routes } from "./routes/index";
+import { errorHandling } from "./middlewares/error-handling";
 
 const app = express();
 const PORT = 3333;
 
 app.use(express.json());
 app.use(routes)
+
+app.use(errorHandling)
 
 app.listen(PORT, () => 
   console.log(`Server is running on http://localhost:${PORT}`)
