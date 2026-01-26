04df45638001aa5941a75b55da3ca6c9635fe4a7
Wellington Barbosa <huehueb58@gmail.com>
2026-01-26 19:49:45 -0300

    adiciona configuração do knex para uso com sqlite3

---
+++ knexfile.ts
@@ -0,0 +1,15 @@
+export default {
+  client: "sqlite3",
+  connection: {
+    filename: "./src/database/database.db",
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extensions: "ts",
+    directory: "./src/database/migrations",
+  },
+  seeds: {
+    extensions: "ts",
+    directory: "./src/database/seeds",
+  },
+};
