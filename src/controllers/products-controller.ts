6d939bef18499b51595c447ead603fc0c249b966
Wellington Barbosa <huehueb58@gmail.com>
2026-01-26 21:37:50 -0300

    adiciona método update no ProductController para atualizar produtos

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -34,6 +34,44 @@ class ProductController {
       next(error);
     }
   }
+
+  async update(request: Request, response: Response, next: NextFunction) {
+    try {
+      const id = z
+        .object({
+          id: z
+            .string()
+            .transform((value) => Number(value))
+            .refine((value) => !isNaN(value), {
+              message: "id must be a number",
+            }),
+        })
+        .parse(request.params);
+
+      const bodySchema = z.object({
+        name: z.string().trim().min(6),
+        price: z.number().gt(0),
+      });
+
+      const { name, price } = bodySchema.parse(request.body);
+
+      await knex<ProductRepository>("products")
+        .update({
+          name,
+          price,
+          updated_at: knex.fn.now(),
+        })
+        .where(id);
+
+      const productUpdated = await knex<ProductRepository>("products")
+        .select()
+        .where(id);
+
+      return response.status(200).json(productUpdated);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 export { ProductController };
--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -6,5 +6,6 @@ const productsController = new ProductController();
 
 productsRoutes.get("/", productsController.index);
 productsRoutes.post("/", productsController.create);
+productsRoutes.put("/:id", productsController.update);
 
 export { productsRoutes };
