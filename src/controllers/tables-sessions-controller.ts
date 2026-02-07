d6a386b96c23c8d0083f0d3aef05691d2d251ca7
Wellington Barbosa <huehueb58@gmail.com>
2026-02-07 14:54:33 -0300

    adiciona validação e verificação de sessão no método create do controlador de sessões de mesas

--- src/controllers/tables-sessions-controller.ts
+++ src/controllers/tables-sessions-controller.ts
@@ -1,6 +1,7 @@
 import { knex } from "@/database/knex";
 import { z } from "zod";
 import { Request, Response, NextFunction } from "express";
+import { AppError } from "@/utils/AppError";
 
 class TablesSessionsController {
   async index(request: Request, response: Response, next: NextFunction) {
@@ -16,10 +17,20 @@ class TablesSessionsController {
         table_id: z.number(),
       });
       const { table_id } = bodySchema.parse(request.body);
+      const session = await knex<TableSessionsRepository>("tables_sessions")
+        .where({ table_id })
+        .orderBy("opened_at", "desc")
+        .first();
+
+      if (session && !session.closed_at) {
+        throw new AppError("Table already opened");
+      }
+
       await knex<TableSessionsRepository>("tables_sessions").insert({
         table_id,
         opened_at: knex.fn.now(),
       });
+
       return response.status(201).json();
     } catch (error) {
       next(error);
