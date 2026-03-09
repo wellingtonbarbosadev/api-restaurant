3316a50a8a60e30cec8d7d5987f8b58e9bda3eeb
Wellington Barbosa <huehueb58@gmail.com>
2026-03-09 10:58:34 -0300

    adiciona método para calcular total de pedidos por sessão de mesa

--- src/routes/orders-routes.ts
+++ src/routes/orders-routes.ts
@@ -6,6 +6,10 @@ const ordersController = new OrdersController();
 
 ordersRoutes.get("/", ordersController.index);
 ordersRoutes.get("/table-session/:table_session_id", ordersController.index);
+ordersRoutes.get(
+  "/table-session/:table_session_id/total",
+  ordersController.show,
+);
 ordersRoutes.post("/", ordersController.create);
 
 export { ordersRoutes };
